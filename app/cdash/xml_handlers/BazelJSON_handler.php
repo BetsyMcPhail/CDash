f2017a497796c1dbda70a1c05c6c13aaa9c4b06b
BetsyMcPhail <betsy.mcphail@kitware.com>
2020-05-09 19:18:46 +0000

    More changes

--- app/cdash/xml_handlers/BazelJSON_handler.php
+++ app/cdash/xml_handlers/BazelJSON_handler.php
@@ -382,11 +382,7 @@ class BazelJSONHandler extends NonSaxHandler
                         } else {
                             // Done with configure, parsing build errors and warnings
                             $record_error = false;
-                            if (!is_null($build_error) && $type === 0 && empty($build_error->PostContext)) {
-                                // Assume all errors have at least one line of
-                                // context *AND* that the first line of context
-                                // may match the error pattern
-                            } elseif (preg_match($warning_pattern, $line, $matches) === 1
+                            if (preg_match($warning_pattern, $line, $matches) === 1
                                   && count($matches) === 3) {
                                 // This line contains a warning.
                                 $record_error = true;
